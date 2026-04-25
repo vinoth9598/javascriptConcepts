ad0b501893882960073caef4afe57a23a6c87717
vinoth9598 <pvinoth9598@gmail.com>
2026-04-25 12:04:18 +0530

    datatypes in js

---
+++ datatypes.js
@@ -0,0 +1,44 @@
+//Data Types in JavaScript
+/*
+JS Dynamic Programming
+ 
+String
+Number   eg:  1.25,25
+Boolean  eg:  True,False
+Null
+Undefinded 
+Symbols  E6
+ 
+ 
+Array
+Object Literals
+Date
+*/
+ 
+var a=25.5;
+var fname="Tutor Joes";
+var isMarried=true;
+var phone=null;
+let b;
+console.log(typeof b);
+ 
+ 
+//ES6 2015
+ 
+const s1=Symbol() //dlkfngsgs6565df6
+console.log(s1)
+ 
+const s2=Symbol() //fdfgdfg4345345
+console.log(s2)
+ 
+console.log(s1==s2);
+ 
+ 
+var courses=['C','C++','Java'];
+var student={
+  'name':'Joes',
+  'age':22
+}
+var d=new Date();
+console.log(d);
+console.log(typeof d);
